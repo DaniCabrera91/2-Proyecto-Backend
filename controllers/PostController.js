2e58fa48f188af3790b4f2b084605e6bdc2020b7
DaniCabrera91 <danielcabreravazquez91@gmail.com>
2024-09-25 01:00:10 +0200

    Made modifications to adapt the post controller code in the implementation of likes and to fix the part of editing posts or deleting them

--- controllers/PostController.js
+++ controllers/PostController.js
@@ -1,19 +1,19 @@
-const cloudinary = require('../config/cloudinaryConfig')
-const Post = require("../models/Post")
-const User = require("../models/User")
+const cloudinary = require('../config/cloudinaryConfig');
+const Post = require("../models/Post");
+const User = require("../models/User");
 
 const PostController = {
   async create(req, res) {
     try {
-      const { title, body } = req.body
+      const { title, body } = req.body;
 
       if (!title || !body) {
-        return res.status(400).send({ message: 'El título y el cuerpo del post son obligatorios.' })
+        return res.status(400).send({ message: 'El título y el cuerpo del post son obligatorios.' });
       }
 
-      let imageUrl
+      let imageUrl;
       if (req.file) {
-        imageUrl = req.file.path
+        imageUrl = req.file.path;
       }
 
       const post = await Post.create({
@@ -21,50 +21,57 @@ const PostController = {
         body,
         userId: req.user._id,
         imageUrl,
-      })
+      });
 
-      await User.findByIdAndUpdate(req.user._id, { $push: { posts: post._id } })
+      await User.findByIdAndUpdate(req.user._id, { $push: { posts: post._id } });
 
-      res.status(201).send({ message: "Post creado con éxito", post })
+      res.status(201).send({ message: "Post creado con éxito", post });
     } catch (error) {
-      console.error(error)
-      res.status(500).send({ message: 'Error al crear el post. Intenta de nuevo más tarde.', error: error.message })
+      console.error(error);
+      res.status(500).send({ message: 'Error al crear el post. Intenta de nuevo más tarde.', error: error.message });
     }
   },
 
   async update(req, res) {
     try {
-      const updateData = { ...req.body }
+      const postId = req.params._id;
+      const updateData = { ...req.body };
 
+      // Verifica si se ha proporcionado un nuevo archivo
       if (req.file) {
-        const result = await cloudinary.uploader.upload(req.file.path)
-        if (!result || !result.secure_url) {
-          return res.status(500).send({ message: 'No se ha podido subir la imagen a Cloudinary. Intenta de nuevo más tarde.' })
-        }
-        updateData.imageUrl = result.secure_url
+        // Subir la nueva imagen a Cloudinary
+        const result = await cloudinary.uploader.upload(req.file.path);
+        updateData.imageUrl = result.secure_url; // Almacena la nueva URL segura
       }
 
-      const post = await Post.findByIdAndUpdate(req.params._id, updateData, { new: true })
+      // Actualiza el post en la base de datos
+      const post = await Post.findByIdAndUpdate(postId, updateData, { new: true });
       if (!post) {
-        return res.status(404).send({ message: 'Post no encontrado.' })
+        return res.status(404).send({ message: 'Post no encontrado.' });
       }
-      res.status(200).send({ message: 'Post actualizado con éxito', post })
+
+      // Devuelve la respuesta con el post actualizado
+      res.status(200).send({ message: 'Post actualizado con éxito', post });
     } catch (error) {
-      console.error(error)
-      res.status(500).send({ message: 'Error al actualizar el post. Intenta de nuevo más tarde.', error: error.message })
+      console.error(error);
+      res.status(500).send({ message: 'Error al actualizar el post. Intenta de nuevo más tarde.', error: error.message });
     }
   },
 
   async delete(req, res) {
     try {
-      const deletedPost = await Post.findByIdAndDelete(req.params._id)
+      const deletedPost = await Post.findByIdAndDelete(req.params._id);
       if (!deletedPost) {
-        return res.status(404).send({ message: 'Post no encontrado.' })
+        return res.status(404).send({ message: 'Post no encontrado.' });
       }
-      res.status(200).send({ message: 'Post eliminado con éxito' })
+
+      // Eliminar la referencia del post del modelo de usuario
+      await User.findByIdAndUpdate(req.user._id, { $pull: { posts: deletedPost._id } });
+
+      res.status(200).send({ message: 'Post eliminado con éxito' });
     } catch (error) {
-      console.error(error)
-      res.status(500).send({ message: 'Error al eliminar el post. Intenta de nuevo más tarde.', error: error.message })
+      console.error(error);
+      res.status(500).send({ message: 'Error al eliminar el post. Intenta de nuevo más tarde.', error: error.message });
     }
   },
 
@@ -72,28 +79,28 @@ const PostController = {
     try {
       const posts = await Post.find()
         .populate({ path: 'userId', select: 'username profileImageUrl' })
-        .populate({ path: 'comments', populate: { path: 'userId', select: 'username profileImageUrl' } })
+        .populate({ path: 'comments', populate: { path: 'userId', select: 'username profileImageUrl' } });
 
-      res.status(200).send(posts)
+      res.status(200).send(posts);
     } catch (error) {
-      console.error(error)
-      res.status(500).send({ message: 'Error al obtener posts. Intenta de nuevo más tarde.', error: error.message })
+      console.error(error);
+      res.status(500).send({ message: 'Error al obtener posts. Intenta de nuevo más tarde.', error: error.message });
     }
   },
 
   async getAllPages(req, res) {
     try {
-      const { page = 1, limit = 10 } = req.query
+      const { page = 1, limit = 10 } = req.query;
       const posts = await Post.find()
         .populate({ path: 'userId', select: 'username profileImageUrl' })
         .populate({ path: 'comments', populate: { path: 'userId', select: 'username profileImageUrl' } })
         .limit(parseInt(limit))
-        .skip((parseInt(page) - 1) * parseInt(limit))
+        .skip((parseInt(page) - 1) * parseInt(limit));
 
-      res.status(200).send(posts)
+      res.status(200).send(posts);
     } catch (error) {
       console.error(error);
-      res.status(500).send({ message: 'Error al obtener posts. Intenta de nuevo más tarde.', error: error.message })
+      res.status(500).send({ message: 'Error al obtener posts. Intenta de nuevo más tarde.', error: error.message });
     }
   },
 
@@ -101,18 +108,18 @@ const PostController = {
     try {
       const post = await Post.findById(req.params._id)
         .populate({ path: 'userId', select: 'username profileImageUrl' })
-        .populate({ path: 'comments', populate: { path: 'userId', select: 'username profileImageUrl' } })
+        .populate({ path: 'comments', populate: { path: 'userId', select: 'username profileImageUrl' } });
 
       if (!post) {
-        return res.status(404).send({ message: 'Post no encontrado.' })
+        return res.status(404).send({ message: 'Post no encontrado.' });
       }
-      res.status(200).send(post)
+      res.status(200).send(post);
     } catch (error) {
-      console.error(error)
-      res.status(500).send({ message: 'Error al obtener post por ID. Intenta de nuevo más tarde.', error: error.message })
+      console.error(error);
+      res.status(500).send({ message: 'Error al obtener post por ID. Intenta de nuevo más tarde.', error: error.message });
     }
   },
-  
+
   async getPostsByUser(req, res) {
     try {
       const userId = req.params.userId;
@@ -133,29 +140,37 @@ const PostController = {
 
   async like(req, res) {
     try {
-      const post = await Post.findByIdAndUpdate(req.params._id, { $push: { likes: req.user._id } }, { new: true })
+      const postId = req.params._id;
+      const userId = req.user._id;
+
+      // Verifica si el usuario ya ha dado like
+      const post = await Post.findById(postId);
       if (!post) {
-        return res.status(404).send({ message: 'Post no encontrado.' })
+        return res.status(404).send({ message: 'Post no encontrado.' });
       }
-      res.status(200).send({ message: 'Like dado con éxito', post })
+
+      const hasLiked = post.likes.includes(userId);
+      if (hasLiked) {
+        // Si ya ha dado like, quítalo
+        post.likes.pull(userId);
+      } else {
+        // Si no ha dado like, agrégalo
+        post.likes.push(userId);
+      }
+
+      await post.save();
+
+      res.status(200).send({ message: 'Like actualizado con éxito', post });
     } catch (error) {
-      console.error(error)
-      res.status(500).send({ message: 'Error al dar like al post. Intenta de nuevo más tarde.', error: error.message })
+      console.error(error);
+      res.status(500).send({ message: 'Error al dar like al post. Intenta de nuevo más tarde.', error: error.message });
     }
   },
 
   async removeLike(req, res) {
-    try {
-      const post = await Post.findByIdAndUpdate(req.params._id, { $pull: { likes: req.user._id } }, { new: true })
-      if (!post) {
-        return res.status(404).send({ message: 'Post no encontrado.' })
-      }
-      res.status(200).send({ message: 'Like quitado con éxito', post })
-    } catch (error) {
-      console.error(error)
-      res.status(500).send({ message: 'Error al quitar el like del post. Intenta de nuevo más tarde.', error: error.message })
-    }
+    // Este método se eliminó porque ahora el like se agrega o quita en el mismo método `like`.
+    res.status(400).send({ message: 'Método no permitido.' });
   }
-}
+};
 
-module.exports = PostController
+module.exports = PostController;
